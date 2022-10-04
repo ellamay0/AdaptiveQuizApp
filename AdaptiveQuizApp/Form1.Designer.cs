6ac381af43e63280e5f33cf266d9435dc8e3ef16
ellamay0 <107935615+ellamay0@users.noreply.github.com>
2022-10-04 09:01:53 +0100

    Add project files.

---
+++ AdaptiveQuizApp/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace AdaptiveQuizApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.subjectTb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.titlescreenButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startButton.Location = new System.Drawing.Point(78, 257);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(257, 50);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Biology";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // subjectTb
+            // 
+            this.subjectTb.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.subjectTb.Location = new System.Drawing.Point(26, 98);
+            this.subjectTb.Multiline = true;
+            this.subjectTb.Name = "subjectTb";
+            this.subjectTb.Size = new System.Drawing.Size(361, 334);
+            this.subjectTb.TabIndex = 1;
+            this.subjectTb.Text = "                  Your subjects ";
+            this.subjectTb.TextChanged += new System.EventHandler(this.titleLabel_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(78, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Computer science";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(78, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Psychology";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(131, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Click a subject to start";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.Location = new System.Drawing.Point(5, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(783, 65);
+            this.titleLabel.TabIndex = 6;
+            this.titleLabel.Text = "_____________ Quiz App _____________";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(417, 98);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 271);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "graph??";
+            // 
+            // titlescreenButton
+            // 
+            this.titlescreenButton.BackColor = System.Drawing.SystemColors.Window;
+            this.titlescreenButton.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titlescreenButton.Location = new System.Drawing.Point(512, 391);
+            this.titlescreenButton.Name = "titlescreenButton";
+            this.titlescreenButton.Size = new System.Drawing.Size(163, 41);
+            this.titlescreenButton.TabIndex = 8;
+            this.titlescreenButton.Text = "Return to title screen";
+            this.titlescreenButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 458);
+            this.Controls.Add(this.titlescreenButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.subjectTb);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox subjectTb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button titlescreenButton;
+    }
+}
+
